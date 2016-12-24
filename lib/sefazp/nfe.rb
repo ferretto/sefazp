a4706041ae89c095860fa0050eb014f03e8fd958
Lênio Ferretto <lenio@itbox.com.br>
2016-12-24 14:29:34 -0200

    Alterando faturas (array) para campo único

--- lib/sefazp/nfe.rb
+++ lib/sefazp/nfe.rb
@@ -235,14 +235,11 @@ class Nfe
         items << { "codigo" => item_codigo, "codigo_ean" => item_ean, "descricao" => item_descricao, "ncm_sh" => item_ncm_sh, "cfop" => item_cfop, "unidade" => item_unidade, "quantidade" => item_quantidade, "valor_unitario" => item_valor_unitario, "valor_total" => item_valor_total, "cst" => item_cst, "base_de_calculo_do_icms" => item_base_de_calculo_do_icms, "valor_do_icms" => item_valor_do_icms, "aliquota_do_icms" => item_aliquota_do_icms }
       end
 
-      faturas = []
-      @document.elements.each("nfeProc/NFe/infNFe/cobr/fat") do |fatura|
-      	numero_fatura = fatura.elements['nFat'].get_text.to_s rescue nil
-      	valor_original_fatura = fatura.elements['vOrig'].get_text.to_s rescue nil
-      	valor_liquido_fatura = fatura.elements['vLiq'].get_text.to_s rescue nil
-        faturas << {"numero" => numero_fatura, "valor_original" => valor_original_fatura, "valor_liquido" => valor_liquido_fatura}
-      end
-
+      numero_fatura = @document.elements['nfeProc/NFe/infNFe/cobr/fat/nFat'].get_text.to_s rescue nil
+      valor_original_fatura = document.elements['nfeProc/NFe/infNFe/cobr/fat/vOrig'].get_text.to_s rescue nil
+      valor_liquido_fatura = @document.elements['nfeProc/NFe/infNFe/cobr/fat/vLiq'].get_text.to_s rescue nil
+      fatura = {"numero" => numero_fatura, "valor_original" => valor_original_fatura, "valor_liquido" => valor_liquido_fatura}
+   
       duplicatas = []
       @document.elements.each("nfeProc/NFe/infNFe/cobr/dup") do |duplicata|
       	data_vencimento_duplicata = duplicata.elements['dVenc'].get_text.to_s rescue nil
@@ -304,7 +301,7 @@ class Nfe
               peso_liquido: peso_liquido,
               informacoes_complementares: informacoes_complementares,
               items: items,
-              faturas: faturas,
+              fatura: fatura,
               duplicatas: duplicatas
               }
     rescue
